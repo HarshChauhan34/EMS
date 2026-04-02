7b3a8326a5ced272e749fc0240439e61f7ea0875
HarshChauhan34 <harshchauhan7631@gmail.com>
2026-04-02 17:11:03 +0530

    comments

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,7 +1,6 @@
 import multer from "multer";
 import path from "path";
 
-// Storage config
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/");
@@ -12,7 +11,6 @@ const storage = multer.diskStorage({
   },
 });
 
-// File filter (only images allowed)
 const fileFilter = (req, file, cb) => {
   if (
     file.mimetype === "image/jpeg" ||
