749ef3cd5d49ab81797d3e883673b8063de3cde4
HarshChauhan34 <harshchauhan7631@gmail.com>
2026-04-02 14:41:39 +0530

    deployed

--- frontend/src/pages/Admin/EditEvent.jsx
+++ frontend/src/pages/Admin/EditEvent.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import API from "../../services/api";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_URL?.replace("/api", "") || "http://localhost:5000";
 
 function EditEvent() {
   const { id } = useParams();
--- frontend/src/pages/EventDetails.jsx
+++ frontend/src/pages/EventDetails.jsx
@@ -14,7 +14,7 @@ function EventDetails() {
   const user = JSON.parse(localStorage.getItem("user"));
 
   // ✅ Use ENV (VERY IMPORTANT)
-  const API_URL = "http://localhost:5000/api";
+  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
   const SERVER_URL = API_URL.replace("/api", "");
 
   // ================= FETCH EVENT =================
